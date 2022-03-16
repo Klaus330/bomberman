5aecbe4e5ed341ea799537c945534d825b65d3bc
Puricoi Catalin <catalinpuricoi15@gmail.com>
2022-03-17 00:35:09 +0200

    Player

--- bomberman/Assets/BombSpawner.cs
+++ bomberman/Assets/BombSpawner.cs
@@ -7,18 +7,18 @@ public class BombSpawner : MonoBehaviour
 {
     public Tilemap tilemap;
     public GameObject bombPrefab;
+    public Transform playerPosition;
+    public float countdown = 2f;
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetKey("space"))
         {
-            Vector3 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            Vector3Int cell = tilemap.WorldToCell(worldPosition);
+            Vector3Int cell = tilemap.WorldToCell(playerPosition.position);
             Vector3 cellCenterPosition = tilemap.GetCellCenterWorld(cell);
 
             Instantiate(bombPrefab, cellCenterPosition, Quaternion.identity);
-
         }
     }
 }
