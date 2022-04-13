e0af5db9a9b1a757ffda61b8907a6a06e99d2e56
popaClaudiu <54347198+popaClaudiu@users.noreply.github.com>
2022-04-13 16:43:10 +0300

    Add random powerup spawner

--- bomberman/Assets/Scripts/PowerUpRandomSpawner.cs
+++ bomberman/Assets/Scripts/PowerUpRandomSpawner.cs
@@ -8,33 +8,52 @@ public class PowerUpRandomSpawner : MonoBehaviour
     // Start is called before the first frame update
     public Tilemap tilemapGameplay;
     public Tilemap tilemapDirt;
+    public Tile wallTile;
+    public Tile destructableTile;
     public GameObject bombPrefab;
+    public List<Vector3Int> cells;
+    public List<GameObject> powerUps;
+    public int listCount;
+
+    public float periodicity = 2f;
 
     void Start()
     {
         BoundsInt bounds = tilemapDirt.cellBounds;
         int i = 0;
+        Debug.Log("add cells");
         foreach (Vector3Int pos in tilemapDirt.cellBounds.allPositionsWithin)
         {
-
-            var localPlace = new Vector3Int(pos.x, pos.y, pos.z);
-            Vector3 cellCenterPosition = tilemapDirt.GetCellCenterWorld(localPlace);
-            if (i == 0)
-            {
-                Instantiate(bombPrefab, cellCenterPosition, Quaternion.identity);
-                i++;            
-            }
-                Debug.Log(localPlace);
+            Vector3Int localPlace = new Vector3Int(pos.x, pos.y, pos.z);
+            cells.Add(localPlace);
         }
 
-        //TileBase[] allTiles = tilemapDirt.GetTilesBlock(bounds);
-        //Debug.Log(tilemapDirt.GetCellCenterWorld(allTiles[0]));
-
+        listCount = cells.Count;
+        Debug.Log(listCount);
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if(periodicity <= 0)
+        {
+            int index = Random.Range(0, listCount);
+            Vector3Int cellPositionInt = cells[index];
+            Tile cell = tilemapGameplay.GetTile<Tile>(cellPositionInt);
+
+            if(cell != wallTile && cell != destructableTile)
+            {
+                Vector3 cellCenterPosition = tilemapDirt.GetCellCenterWorld(cellPositionInt);
+
+                index = Random.Range(0, powerUps.Count);
+                GameObject powerupPrefab = powerUps[index];
+
+                Instantiate(powerupPrefab, cellCenterPosition, Quaternion.identity);
+            }
+
+            periodicity = 2f;
+        }
+
+        periodicity -= Time.fixedDeltaTime;
     }
 }
